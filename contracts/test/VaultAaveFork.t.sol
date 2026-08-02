9785f5a3de8c234dd77a8226b55f83675bd99f96
pradyumn100 <pradyumntiwari53@gmail.com>
2026-08-03 00:21:13 +0530

    some final changes done phase 1 completion ft.VaultAaveFork.t.sol

--- contracts/test/VaultAaveFork.t.sol
+++ contracts/test/VaultAaveFork.t.sol
@@ -4,49 +4,45 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {Vault} from "../src/Vault.sol";
 import {AaveAdapter} from "../src/AaveAdapter.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MockAavePool, MockAToken} from "./mocks/MockAavePool.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract VaultAaveForkTest is Test {
-    Vault public vault;
-    AaveAdapter public adapter;
+contract MockDAI is ERC20 {
+    constructor() ERC20("Mock DAI", "mDAI") { _mint(msg.sender, 1_000_000e18); }
+}
 
-    address constant ASSET = 0xFF34B3d4Aee8ddCd6F9AFFFB6Fe49bD371b8a357;  // DAI
-    address constant ATOKEN = 0x29598b72eb5CeBd806C5dCD549490FdA35B13cD8; // aDAI
+contract VaultAaveMockTest is Test {
+    Vault vault;
+    AaveAdapter adapter;
+    MockAavePool pool;
+    MockDAI dai;
     address user = address(0xBEEF);
 
     function setUp() public {
-        vm.createSelectFork(vm.envString("SEPOLIA_RPC_URL"));
+        dai = new MockDAI();
+        pool = new MockAavePool(address(dai));
 
-        vault = new Vault(IERC20(ASSET), "Yield Vault USDC", "yvUSDC", address(0));
-        adapter = new AaveAdapter(ASSET, ATOKEN, address(vault));
+        vault = new Vault(dai, "Yield Vault DAI", "yvDAI", address(0));
+        adapter = new AaveAdapter(address(dai), address(pool.aToken()), address(vault), address(pool));
         vault.setStrategy(address(adapter));
 
-        deal(ASSET, user, 10e18); // adjust decimals if your test asset isn't 6-decimal
+        dai.transfer(user, 1_000e18);
     }
 
-    function testDepositAccrueWithdraw() public {
+    function testDepositAndWithdraw() public {
         vm.startPrank(user);
-        IERC20(ASSET).approve(address(vault), 10e18);
-        vault.deposit(10e18, user);
+        dai.approve(address(vault), 100e18);
+        vault.deposit(100e18, user);
         vm.stopPrank();
 
-        uint256 afterDeposit = vault.totalAssets();
-        assertGt(afterDeposit, 0);
-        console.log("Total assets after deposit:", afterDeposit);
-
-        vm.warp(block.timestamp + 30 days);
-        vm.roll(block.number + 1);
-
-        uint256 afterTime = vault.totalAssets();
-        console.log("Total assets after 30 days:", afterTime);
-        assertGe(afterTime, afterDeposit); // interest should never make this go down
+        assertEq(vault.totalAssets(), 100e18);
+        console.log("Total assets after deposit:", vault.totalAssets());
 
         vm.startPrank(user);
         vault.redeem(vault.balanceOf(user), user, user);
         vm.stopPrank();
 
-        uint256 userBalance = IERC20(ASSET).balanceOf(user);
-        console.log("User balance after withdraw:", userBalance);
-        assertGt(userBalance, 0);
+        assertEq(dai.balanceOf(user), 1_000e18);
+        console.log("User balance after withdraw:", dai.balanceOf(user));
     }
 }
