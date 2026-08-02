3845d23c5e2826878e60b71d4cdbbacb718c647c
pradyumn100 <pradyumntiwari53@gmail.com>
2026-08-02 14:12:01 +0530

    feat(vault): implement ERC-4626 vault core logic

---
+++ contracts/src/Vault.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IStrategyAdapter} from "./IStrategyAdapter.sol";
+
+contract Vault is ERC4626, Ownable {
+    using SafeERC20 for IERC20;
+
+    IStrategyAdapter public activeStrategy;
+
+    constructor(
+        IERC20 asset_,
+        string memory name_,
+        string memory symbol_,
+        address strategy_
+    )
+    
+     ERC4626(asset_) ERC20(name_, symbol_) Ownable(msg.sender) {
+        activeStrategy = IStrategyAdapter(strategy_);
+    }
+    function setStrategy(address newStrategy) external onlyOwner {
+    activeStrategy = IStrategyAdapter(newStrategy);
+}
+
+    function totalAssets() public view override returns (uint256) {
+        return activeStrategy.totalDeposited();
+    }
+
+    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
+        super._deposit(caller, receiver, assets, shares); // pulls assets from depositor into vault
+        IERC20(asset()).forceApprove(address(activeStrategy), assets);
+        activeStrategy.deposit(assets); // forward into Aave
+    }
+
+    function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares) internal override {
+        activeStrategy.withdraw(assets); // pull back from Aave first
+        super._withdraw(caller, receiver, owner, assets, shares);
+    }
+}
