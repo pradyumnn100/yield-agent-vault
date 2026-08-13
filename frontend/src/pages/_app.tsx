74f09426c5a9d9e0669644abe6cb21f2d1881781
pradyumn100 <pradyumntiwari53@gmail.com>
2026-08-13 10:47:10 +0530

    Fix relative import paths in _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,8 +5,8 @@ import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-
-import { config } from '../wagmi';
+import { VaultCard } from "../../components/VaultCard";
+import { config } from '../../wagmi';
 
 const client = new QueryClient();
 
